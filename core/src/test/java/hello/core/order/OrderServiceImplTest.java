cd457954c73c9cce2cca6991f67c31d7ce90c1ae
JSY8869 <65009713+JSY8869@users.noreply.github.com>
2021-10-09 12:16:35 +0900

    공부 기록!
    2021/10/09
    다양한 의존관계 주입 방법
    옵션 처리
    생성자 주입을 선택하라!

---
+++ core/src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -0,0 +1,17 @@
+package hello.core.order;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.MemoryMemberRepository;
+import org.junit.jupiter.api.Test;
+import hello.core.member.Member;
+
+public class OrderServiceImplTest {
+    @Test
+    void createOrder() {
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+        OrderServiceImpl orderService = new OrderServiceImpl(memberRepository, new FixDiscountPolicy());
+        orderService.createOrder(1L, "itemA", 10000);
+    }
+}
