e830fcbbb4b89586a2215ed5efa0b0f4c6e8231d
Jeong SeYoung <65009713+JSY8869@users.noreply.github.com>
2022-01-13 22:04:20 +0900

    주문 개발 완료

--- jpa 활용/jpashop/src/main/java/jpabook/jpashop/domain/item/Book.java
+++ jpa 활용/jpashop/src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -9,7 +9,7 @@ import javax.persistence.Entity;
 @Entity
 @Getter @Setter
 @DiscriminatorValue("")
-public class book extends Item{
+public class Book extends Item{
 
     private String author;
     private String isbn;
