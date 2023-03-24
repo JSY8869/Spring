1f6a0534c45201c399cd93e486535c638351fbd3
Jeong SeYoung <65009713+JSY8869@users.noreply.github.com>
2023-03-24 17:43:11 +0900

    고급편 완료

---
+++ spring 고급/aop/aop/src/test/java/hello/aop/proxyvs/ProxyCastingTest.java
@@ -0,0 +1,36 @@
+package hello.aop.proxyvs;
+
+import hello.aop.member.MemberService;
+import hello.aop.member.MemberServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+
+@Slf4j
+public class ProxyCastingTest {
+
+    @Test
+    void jdkProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(false); // JDK 동적 프록시 사용
+
+        MemberService proxy = (MemberService) proxyFactory.getProxy();
+
+        Assertions.assertThrows(ClassCastException.class, () -> {
+            MemberServiceImpl casting = (MemberServiceImpl) proxy;
+        });
+    }
+
+    @Test
+    void cglibProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true); // CGLIB 프록시 사용
+
+        MemberService proxy = (MemberService) proxyFactory.getProxy();
+
+        MemberServiceImpl casting = (MemberServiceImpl) proxy;
+    }
+}
